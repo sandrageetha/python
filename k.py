90b5d13e3021a77851365177b0cccf26eceb5be9
sandrageetha <121040046+sandrageetha@users.noreply.github.com>
2023-02-28 06:03:20 +0530

    Add files via upload

---
+++ k.py
@@ -0,0 +1,19 @@
+T =int(input())
+for test_case in range(T):
+       classID = input()
+       if (classID == 'B' or classID == 'b'):
+           shipclass ="Battleship" 
+       elif (classID == 'C' or classID == 'c'):
+           shipclass = "Cruiser"
+       elif (classID == 'D' or classID == 'd'):
+           shipclass = "Destroyer"
+       elif (classID == 'F'  or classID == 'f'):
+           shipclass = "Frigate"
+       print(shipclass)
+
+       
+           
+           
+       
+   
+
