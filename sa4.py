a43eb0b8ac29e3d572e7292fc4d9a90855967ec6
sandrageetha <121040046+sandrageetha@users.noreply.github.com>
2023-02-28 07:16:14 +0530

    Add files via upload

---
+++ sa4.py
@@ -0,0 +1,9 @@
+l=list(map(int,input("enter the numbers:").split(',')))
+for i in range(0,len(l)):
+    if(not(l[i]%2)):
+        print(l[i])
+    elif(l[i]==236):
+        
+        break
+    
+        
